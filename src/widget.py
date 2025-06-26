4bb848878ecf57ee3ca5572a9c9a3f5a465c0b23
Maria Ivanova <masha05101991@gmail.com>
2025-06-26 18:32:50 +0300

    Добавление новых функций в модуль widget.py

---
+++ src/widget.py
@@ -0,0 +1,20 @@
+from src.masks import get_mask_card_number
+
+
+def mask_account_card(card_info: str) -> str:
+    """Функция определяет счет это или номер карты и возвращает замаскированный"""
+    card_info_list = card_info.split()
+    if "Счет" in card_info_list:
+        return f"Счет **{card_info_list[-1][-4:]}"
+    else:
+        card_name = " ".join(card_info_list[:-1])
+        card_number = card_info_list[-1].replace(" ", "")
+        return f"{card_name} {get_mask_card_number(card_number)}"
+
+
+def get_date(date_str: str) -> str:
+    """Преобразует дату в формат ДД.ММ.ГГГГ"""
+    date_list = date_str.split("-", 2)
+    date_list[2] = date_list[2][:2]
+    date_list_reverse = ".".join(date_list[::-1])
+    return date_list_reverse
