fdeaa5f52f088cee2e949e73a25c9ce2adf70db3
Maria Ivanova <masha05101991@gmail.com>
2025-07-24 12:07:33 +0300

    создание функции filter_by_currency

--- src/generators.py
+++ src/generators.py
@@ -1,1 +1,8 @@
-def filter_by_currency(transactions, currency_code):
+from typing import List, Dict, Iterable
+
+
+def filter_by_currency(transactions: List[Dict], currency: str) -> Iterable:
+    """Возвращает итератор транзакций с указанной валютой"""
+    for transaction in transactions:
+        if transaction["operationAmount"]["currency"]["code"] == currency:
+            yield transaction
