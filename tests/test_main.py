8bf2213cf215869150b7fdfc6ba31b94689cc558
Maria Ivanova <masha05101991@gmail.com>
2025-10-01 10:15:00 +0300

    исправление ошибок

--- tests/test_main.py
+++ tests/test_main.py
@@ -6,10 +6,7 @@ import io
 
 @pytest.fixture
 def patch_input_and_files():
-    inputs = iter([
-        "1",
-        "test.json"
-    ])
+    inputs = iter(["1", "test.json"])
 
 
 def test_main_flow(patch_input_and_files, capsys):
@@ -19,9 +16,10 @@ def test_main_flow(patch_input_and_files, capsys):
 
 
 def test_get_transactions_from_json():
-    fake_json_content = ('[{"date": "01.01.2022", "description": '
-                         '"Покупка", "amount": "5000 руб.", "status": "EXECUTED"}]')
-    with patch("builtins.open", return_value=io.BytesIO(fake_json_content.encode('utf-8'))):
+    fake_json_content = (
+        '[{"date": "01.01.2022", "description": ' '"Покупка", "amount": "5000 руб.", "status": "EXECUTED"}]'
+    )
+    with patch("builtins.open", return_value=io.BytesIO(fake_json_content.encode("utf-8"))):
         result = main.get_transactions_from_json("dummy_path.json")
         assert isinstance(result, list)
         assert result[0]["description"] == "Покупка"
